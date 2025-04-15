660c81d56b9792f89b6a850661a531f7f5abd477
Better Bet <matchedbettingtipstersuk@gmail.com>
2025-04-15 23:00:12 +0100

    adjusting add lock if premium function

--- oddsmatchers/standardFREE/myOddsmatcher.js
+++ oddsmatchers/standardFREE/myOddsmatcher.js
@@ -638,35 +638,41 @@
         
         
     add_lock_if_premium() {
-      
+
+        // adjuts this function across all oddsmatchers
+  
         if (!is_premium_member && filteredData.length != 0) {
+
+            this.shadowRoot.querySelector('table tbody').classList.add("blurred_tbody");
+
             const placeholderRow = document.createElement('tr');
             placeholderRow.innerHTML = `
-    
+   
                 <td colspan="100%" class="not_premium_member_row" >
                     <div class="outer_div_upgrade" >
-    
-    
+   
+   
                             <div class="outer_div_upgrade_text_row" >
-    
-                                <a class="upgrade-button">Upgrade to Premium <img src="https://img.icons8.com/?size=100&id=60654&format=png&color=ffffff" class="padlock-image-button" alt="Padlock" > </a>
-    
+      
+                                <a class="upgrade-button">Upgrade to Premium <svg fill="#ffffff" class="padlock-image-button" alt="Padlock" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xml:space="preserve" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="XMLID_509_"> <path id="XMLID_510_" d="M65,330h200c8.284,0,15-6.716,15-15V145c0-8.284-6.716-15-15-15h-15V85c0-46.869-38.131-85-85-85 S80,38.131,80,85v45H65c-8.284,0-15,6.716-15,15v170C50,323.284,56.716,330,65,330z M180,234.986V255c0,8.284-6.716,15-15,15 s-15-6.716-15-15v-20.014c-6.068-4.565-10-11.824-10-19.986c0-13.785,11.215-25,25-25s25,11.215,25,25 C190,223.162,186.068,230.421,180,234.986z M110,85c0-30.327,24.673-55,55-55s55,24.673,55,55v45H110V85z"></path> </g> </g></svg> </a>
+   
                             </div>
-    
+   
                     </div>
-    
+   
                    
                 </td>
-    
+   
             `;
-    
-            const tableBody = this.shadowRoot.querySelector('table tbody');
+   
+            const tableBody = this.shadowRoot.querySelector('table');
             tableBody.append(placeholderRow);
-    
-           
+      
+        } else {
+            this.shadowRoot.querySelector('table tbody').classList.remove("blurred_tbody");
         }
-    
-    
+   
+   
     }
     
     
